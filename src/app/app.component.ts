033e2280cc322804e33e674e18a911031c482e90
Dunridge <Polinka7max@gmail.com>
2021-02-20 12:00:49 +0200

    defined a working reducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppState} from './store/models/app-state.model';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {ShoppingItem} from './store/models/shopping-item.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  shoppingItems$: Observable<ShoppingItem[]>;
   title = 'ngrx-shopping-list';
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit(): void {
+    this.shoppingItems$ = this.store.select(store => store.shopping); // in future this will be replaced with a selector
+  }
 }
